9c0a2c49676b4fbf9e0b5087883e9b244cd95df4
Liora <liora.fortuna@students.mikroskil.ac.id>
2025-07-26 12:49:17 +0700

    Initial commit

--- app/components/StartupCard.tsx
+++ app/components/StartupCard.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { EyeIcon } from 'lucide-react';
 import { formatDate } from '@/lib/utils';
 import Link from 'next/link';
+import Image from 'next/image';
+import Button from './ui/Button';
+import { createFetch } from 'next/dist/client/components/router-reducer/fetch-server-response';
 
 
 const StartupCard = ({post}: {post: StartupTypeCard}) => {
-  const { _createdAt, views, author:{_id, name}, title, category, _id, image } = post;
+  const { _createdAt, views, author:{_id:authorId, name}, title, category, _id, image, description } = post;
 
   return (
     <li className='startup-card group'>
         <div className='flex-between'>
             <p className='startup_card_date'>
-                {formatDate(post._createdAt)}
+                {formatDate(_createdAt)}
             </p>
             <div className='flex gap-1.5'>
                 <EyeIcon className='size-6 text-primary'/>
@@ -21,11 +24,33 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
 
         <div className='flex-between mt-5 gap-5'>
             <div className='flex-1'>
-                <Link href={`/user/${post.author?._id}`}>
+                <Link href={`/user/${authorId}`}>
                     <p className='text-16-medium line-clamp-1'>
-                        {post.author?.name}
+                        {name}
                     </p>
                 </Link>
+                <Link href={`/startup/${_id}`}>
+                    <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
+                </Link>
+                <Link href={`/user/${authorId}`}>
+                    <Image src='https://placehold.co/48x48' alt='placeholder' width={48} height={48} className='rounded-full'/>
+                </Link>
+            </div>
+            <Link href={`/startup/${_id}`}>
+                <p className='startup-card_desc'>
+                    {description}
+                </p>
+
+                <img src={image} alt="placeholder" className='startup-card_img' />
+            </Link>
+
+            <div className='flex-between gap-3 mt-5'>
+                <Link href={`/?query=${category.toLowerCase()}`}>
+                    <p className='text-16-medium'>{category}</p>
+                </Link>
+                <Button className="startup-card_btn" asChild>
+                    <Link href={`/startup/${_id}`}></Link>
+                </Button>
             </div>
         </div>
     </li>
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString(locales:'en-US', options: {
+  return new Date(date).toLocaleDateString('en-US', {
     month: 'long',
     day: 'numeric',
     year: 'numeric'
-  })
+  });
 }
